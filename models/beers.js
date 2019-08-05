2f514ea2e256d6bc042570c6b5239322c93d9ea8
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-08-05 13:45:51 -0600

    Initial styling complete

--- models/beers.js
+++ models/beers.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const beerSchema = new mongoose.Schema({
   name: {type: String, required: true},
+  type: {type: String},
   brewery: { type: String},
-  image: {type: String},
+  image: {type: String, required: true},
   description: {type: String},
-  notes: String
+  rating: {type: Number},
+  notes: {type: String}
 });
 
 const Beer = mongoose.model('beers', beerSchema);
--- models/seed_data.js
+++ models/seed_data.js
@@ -1,72 +1,92 @@
 module.exports = [
   {
     name: 'Vanilla Porter',
+    type: 'Porter',
     brewery: 'Breckenridge Brewery',
-    image: 'https://www.breckbrew.com/_photos/brews/Vanilla%20Porter%20-%20Refresh%2012oz%20BTL.png',
+    image: 'https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/hf1/hc3/8809263824926.png',
     description: 'Real vanilla beans from Madagascar combined with caramel and Munich malts create a beer that’s balanced and flavorful. Aromas of vanilla and toasted grain set the stage for mellow flavors of vanilla and dark roasted malts. Don’t let its deep mahogany color fool you. Vanilla Porter is dark in color and packed with flavor, yet it is as smooth as they come and easy to enjoy at any time of the year.',
+    rating: 8,
     notes: 'Nice Porter'
   },
   {
     name: 'Fat Tire',
+    type: 'Amber Ale',
     brewery: 'New Belgium Brewery',
     image: 'https://beerconnoisseur.com/sites/default/files/styles/beer_page_245w/public/beer/fat-tire.jpg?itok=4gQqokyA',
     description: 'Fat Tire is a classic amber ale with biscuit notes and a soft warming alcohol touch in the aroma and flavor. The hops are not your typical citrus hops, but they are still refreshing and crisp.',
-    notes: 'Great abmer ale'
+    rating: 9,
+    notes: 'Great amber ale'
   },
   {
     name: 'Extra Special Bitter',
+    type: 'Ale',
     brewery: 'Breckenridge Brewery',
     image: 'https://cdn.beeradvocate.com/im/beers/180717.jpg',
     description: 'The balance of malt and hops makes it truly one-of-a-kind. Warming malt notes at the onset with a hop kick in the back and opens up to a nice dry finish. Our small batch ESB is a big traditional ale with characteristics that break tradition and a complexity that constantly surprises.',
+    rating: 7,
     notes: 'Very bitter'
   },
   {
-    name: 'Avalanche Amber Ale',
+    name: 'Avalanche',
+    type: 'Amber Ale',
     brewery: 'Breckenridge Brewery',
     image: 'https://beerconnoisseur.com/sites/default/files/styles/beer_page_245w/public/beer/breckenridge-brewery-avalanche-amber-ale.jpg?itok=ehAJlukI',
     description: 'This classic American amber ale blends pale, chocolate, and caramel malts to create a well-rounded, flavorful beer. Caramel malts lend whispers of toffee and an enjoyable sweetness, while a touch of bittering hops gives this beer its clean finish. Crafted for balance, Avalanche is a versatile and satisfying anytime beer.',
+    rating: 9,
     notes: 'Smooth'
   },
   {
-    name: 'VooDoo Ranger Imperial IPA',
+    name: 'VooDoo Ranger',
+    type: 'Imperial IPA',
     brewery: 'New Belgium Brewery',
-    image: 'https://www.newbelgium.com/globalassets/beer/voodoo-ranger-imperial/vrimp_product.png',
+    image: 'https://decrescente.net/images/suppliers/new-belgium/new-belgium-voodoo-ranger-imperial/voodoo-ranger-imperial-bottle-lg.png',
     description: 'New Belgium Voodoo Ranger Imperial IPA is a rare blend of choice hops that creates an explosion of fresh-cut pine and citrus flavors for a complex, rich and delicious flavor.',
+    rating: 8,
     notes: 'Pairs well with spicy food'
   },
   {
-    name: 'Laughing Lab Scottish Ale',
+    name: 'Laughing Lab',
+    type: 'Scottish Ale',
     brewery: 'Bristol Brewery',
     image: 'http://www.bristolbrewing.com/wp-content/uploads/2016/08/laughing-lab-bottle_glass-1.png',
     description: 'Laughing Lab is our most popular brew among beer drinkers and beer judges alike, with nine medals to its name since 1994 at the Great American Beer Festival alone. The Lab has a complex maltiness that comes from a careful blend of specialty malts. Its nutty, roasted flavor is mildly sweet, but not too rich. Smooth and luscious!',
+    rating: 7,
     notes: 'Rich flavor'
   },
   {
-    name: 'Mass Transit Amber Ale',
+    name: 'Mass Transit',
+    type: 'Amber Ale',
     brewery: 'Bristol Brewery',
     image: 'http://www.bristolbrewing.com/wp-content/uploads/2016/08/masstransitcombo.png',
     description: 'Like the bus on its label, Mass Transit has a mellow vibe and a fiercely loyal following of fans. Loosely based on the amber style, this is a dependable session beer that simply delivers superb flavor. Crystal and Vienna specialty malts give Mass its rich auburn color and easygoing nature.',
+    rating: 7,
     notes: 'Mellow'
   },
   {
-    name: 'Red Baron Octoberfest Vienna-Style Lager',
+    name: 'Red Baron Octoberfest',
+    type: 'Lager',
     brewery: 'Bristol Brewing',
     image: 'http://www.bristolbrewing.com/wp-content/uploads/2015/10/Red-Baron_PPMLV_masked-no-glass.png',
     description: 'A hardy beer befitting the celebration and the man it commemorates, Red Baron Octoberfest is a dark lager brewed in the traditional Vienna style. Vienna and Munich malts give our Octoberfest a toasty, dry flavor and ruddy brown color that warm the heart when the leaves begin to fall.',
+    rating: 7,
     notes: 'Hardy'
   },
   {
-    name: '5 Barrel Pale Ale',
+    name: '5 Barrel',
+    type: 'Pale Ale',
     brewery: 'Odell Brewing Company',
     image: 'https://www.jacobliquor.com/images/labels/odell-5-barrel-pale-ale.gif',
     description: 'Named for the 5 select hop additions diligently administered by our brewers throughout the process, we start with an infusion of fresh whole-flower hops in the Hop Back and Fermentor. Then we go four more rounds during the kettle boil, giving this refreshing pale ale a lively flavor and aroma that always keeps us coming back for more. ',
+    rating: 8,
     notes: 'Lively'
   },
   {
-    name: 'Cutthroat Porter',
+    name: 'Cutthroat',
+    type: 'Porter',
     brewery: 'Odell Brewing Company',
     image: 'https://products0.imgix.drizly.com/ci-odell-cutthroat-porter-a62e0ca9fa29fd69.png?auto=format%2Ccompress&fm=jpeg&q=20',
     description: 'Not quite a stout but definitely no lightweight, Cutthroat is smooth, robust and inspired by classic London porters. We use dark roasted malts to create a deep, rich color and flavor that hint at chocolate and coffee. And while we love and respect the London porter, we made sure to give it a little Colorado twist by naming it after our state fish.',
+    rating: 8,
     notes: 'Robust'
   }
 
